fd9824d534247512f40d6ecc8642f90cb1fae7fd
Andy-CSKim <47920625+Andy-CSKim@users.noreply.github.com>
2024-04-21 21:51:16 +0900

    mile2km

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { getRoot } from './fetch';
 
 function App() {
   const [msg, setMsg] = useState('');
+  const [mile, setMile] = useState(0.0);
+  const [km, setKm] = useState(0.0);
 
   const clickRoot = () => {
     console.log('getRoot');
@@ -14,15 +16,19 @@ function App() {
       setMsg(JSON.stringify(data));
     });
   }
+  const cvtMile = () => {
+    console.log(`mile = ${mile}`);
+    setKm(mile * 1.60934);
+  }
 
   return (
     <div className="App">
-      <button onClick={clickRoot}>localhost:3001/</button>
-      <br />
-      <br />
-
-      <p>{msg}</p>
-
+      <button onClick={clickRoot}>localhost:3001/</button><br />
+      <p>{msg}</p><br />
+      <input type="text" value={mile} onChange={(e) => setMile(e.target.value)} />
+      <button onClick={cvtMile}>Mile to Km</button><br />
+      {/* interger to string */}
+      <p>{km.toString()}</p><br />
     </div>
   );
 }
